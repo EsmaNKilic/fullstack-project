999fb1dc97f2121508381d8c17c6c4af2712a745
Esma <50068550+EsmaNKilic@users.noreply.github.com>
2023-09-26 14:00:33 +0300

    interceptors

--- src/app/features/product/components/product-card/product-card.component.ts
+++ src/app/features/product/components/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-card',
@@ -10,4 +11,14 @@ export class ProductCardComponent {
   @Input() title!: string;
   @Input() description!: string;
   @Input() price!: number;
+  @Input() id!: number;
+  @Output() onDelete = new EventEmitter();
+
+  constructor(private productService: ProductService) {}
+
+  deleteProduct() {
+    this.productService.delete(this.id).subscribe((response) => {
+      this.onDelete.emit();
+    });
+  }
 }
