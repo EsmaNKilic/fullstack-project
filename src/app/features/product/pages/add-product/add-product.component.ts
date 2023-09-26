06a8f0ae2ea1f90f14b7533d0191601f683f00cb
Esma <50068550+EsmaNKilic@users.noreply.github.com>
2023-09-26 11:57:36 +0300

    add product form submit

--- src/app/features/product/pages/add-product/add-product.component.ts
+++ src/app/features/product/pages/add-product/add-product.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { CategoryService } from 'src/app/features/category/services/category.service';
 import { Category } from 'src/app/features/category/models/category';
+import { SupplierService } from 'src/app/features/supplier/services/supplier.service';
+import { Supplier } from 'src/app/features/supplier/models/supplier';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GetAllProductsModel } from '../../models/getAllProductsModel';
 
 @Component({
   templateUrl: './add-product.component.html',
@@ -16,34 +20,80 @@ import { Category } from 'src/app/features/category/models/category';
 export class AddProductComponent implements OnInit {
   addProductForm!: FormGroup;
   categories: Category[] = [];
+  suppliers: Supplier[] = [];
+  productToEdit!: GetAllProductsModel;
   constructor(
     private formBuilder: FormBuilder,
     private productService: ProductService,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private supplierService: SupplierService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {}
   ngOnInit(): void {
-    // formda gerekecek db bilgilerini çek / build form
-    // kategoriler => veritabanın
     this.fetchCategories();
+    this.fetchSuppliers();
+
+    this.activatedRoute.params.subscribe((parameters) => {
+      if (parameters['id']) {
+        // ilgili id ile productin detaylarını çek.
+        // bu detaylar ile birlikte formu oluştur.
+        this.productService.getById(parameters['id']).subscribe((response) => {
+          this.productToEdit = response;
+          this.buildForm();
+        });
+      } else {
+        this.buildForm();
+      }
+    });
+
+    // sayfa urlinde id gönderilmişse => update
+    // gönderilmemişse => add
+  }
+
+  fetchSuppliers() {
+    this.supplierService.getAll().subscribe((response: Supplier[]) => {
+      this.suppliers = response;
+    });
   }
 
   fetchCategories() {
     this.categoryService.getAll().subscribe((response: Category[]) => {
       this.categories = response;
-      this.buildForm();
     });
   }
 
   submit() {
     this.addProductForm.markAllAsTouched();
-    console.log(this.addProductForm.value);
+    if (this.addProductForm.invalid) {
+      alert('Validasyon hatası');
+      return;
+    }
+    if (this.productToEdit) {
+      // TODO: Aradan sonra backend-frontend yazalım..
+    } else {
+      this.productService
+        .add(this.addProductForm.value)
+        .subscribe((response) => {
+          alert('Ürün başarıyla eklendi');
+          this.router.navigateByUrl('/product');
+        });
+    }
   }
 
   buildForm() {
     this.addProductForm = this.formBuilder.group({
-      productName: new FormControl('', [Validators.required]),
-      supplierId: new FormControl(0),
-      categoryId: new FormControl(0),
+      productName: new FormControl(this.productToEdit?.name || '', [
+        Validators.required,
+      ]),
+      supplierId: new FormControl(
+        this.productToEdit?.supplier.supplierId || 0,
+        [Validators.min(1)]
+      ),
+      categoryId: new FormControl(
+        this.productToEdit?.category.categoryId || 0,
+        [Validators.min(1)]
+      ),
     });
   }
 }
--- src/app/features/product/services/product.service.ts
+++ src/app/features/product/services/product.service.ts
@@ -3,15 +3,26 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { GetAllProductsModel } from '../models/getAllProductsModel';
+import { AddProductModel } from '../models/addProductModel';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  controllerUrl: string = environment.BASE_API_URL + 'products';
+  private controllerUrl: string = environment.BASE_API_URL + 'products';
   constructor(private httpClient: HttpClient) {}
 
   getAll(): Observable<GetAllProductsModel[]> {
     return this.httpClient.get<GetAllProductsModel[]>(this.controllerUrl);
+  }
+
+  getById(id: number): Observable<GetAllProductsModel> {
+    return this.httpClient.get<GetAllProductsModel>(
+      this.controllerUrl + '/getById?id=' + id
+    );
+  }
+
+  add(model: AddProductModel) {
+    return this.httpClient.post(this.controllerUrl, model);
   }
 }
